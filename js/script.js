6fbaaaf4135fd983139d5772dafeb00c1c875212
DamianoLandi <damianolandi01@gmail.com>
2021-07-29 16:55:16 +0200

    Creazione Lista

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const root = new Vue({
+    el: "#root",
+    data:{
+        list:[
+            "Comprare il pane",
+            "Andare alle poste",
+            "Ricaricare il telefono",
+            "Ripassare HTML e CSS",
+        ]
+    },
+    methods:{
+
+    },
+}); 
