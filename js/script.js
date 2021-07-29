bd71c8a5062878d82ba2f9d0852db36d3bcf13c5
DamianoLandi <damianolandi01@gmail.com>
2021-07-29 17:09:45 +0200

    Input per aggiungere elementi

--- js/script.js
+++ js/script.js
@@ -6,11 +6,18 @@ const root = new Vue({
             "Andare alle poste",
             "Ricaricare il telefono",
             "Ripassare HTML e CSS",
-        ]
+        ],
+        newItem:"",
     },
     methods:{
         removeItem(i){
             this.list.splice(i, 1);
-        }
+        },
+        addItem(){
+            if(this.newItem.trim() != "" && !this.list.includes(this.newItem)){
+                this.list.push(this.newItem);
+            };
+            this.newItem = "";
+        },
     },
 }); 
